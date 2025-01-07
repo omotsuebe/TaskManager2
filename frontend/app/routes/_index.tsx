41978d81a5a2a91dcba9ee7e38949b19d7b39c6b
Hezekiah <hezecom@gmail.com>
2025-01-06 18:36:32 -0600

    fix mobile responsive

--- frontend/app/routes/_index.tsx
+++ frontend/app/routes/_index.tsx
@@ -25,7 +25,7 @@ export default function Index() {
               <Link to="#" className="text-sm/6 font-semibold text-gray-900">Product</Link>
               <Link to="#" className="text-sm/6 font-semibold text-gray-900">Features</Link>
             </div>
-            <div className="hidden lg:flex lg:flex-1 lg:justify-end">
+            <div className="flex flex-1 justify-end">
               {user ? (
               <Link to="/dashboard" className="text-sm font-semibold text-gray-900">Dashboard</Link>
               ) : (
@@ -43,10 +43,10 @@ export default function Index() {
                 className="relative left-[calc(50%-11rem)] aspect-[1155/678] w-[36.125rem] -translate-x-1/2 rotate-[30deg] bg-gradient-to-tr from-[#bfdbfe] to-[#e0e7ff] opacity-30 sm:left-[calc(50%-30rem)] sm:w-[72.1875rem]"></div>
           </div>
           <div className="mx-auto max-w-2xl py-32 sm:py-48 lg:py-56">
-          <div className="hidden sm:mb-8 sm:flex sm:justify-center">
-              <div className="relative rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20">
+          <div className="sm:mb-8 sm:flex sm:justify-center">
+            <div className="relative  rounded-full px-3 py-1 text-sm/6 text-gray-600 ring-1 ring-gray-900/10 hover:ring-gray-900/20 mb-4">
               Want to see how it works. <Link to="/dashboard" className="font-semibold text-indigo-600"><span className="absolute inset-0" aria-hidden="true"></span>Learn more <span aria-hidden="true">&rarr;</span></Link>
-              </div>
+            </div>
             </div>
             <div className="text-center">
               <h1 className="text-balance text-2xl font-semibold tracking-tight text-gray-900 sm:text-xl">Manage your day to day tasks with ease</h1>
